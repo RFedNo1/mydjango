d70a116b9aec2dd6fe9e1c141b958bc325e12de7
RFedNo1 <patrick-rafter@live.jp>
2020-01-30 09:22:36 +0900

    view template

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,28 @@
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Choice, Question
+from django.views import generic
 
 
-def index(request):
-    # ex: /polls/
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the lsat five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # ex: /polls/5/
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    # ex: /polls/5/results/
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/result.html', {'question': question})
+class ResultView(generic.ResultView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
